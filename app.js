fe571ccef2cc5894b63fb45ac5a033df9e3ce2d6
Rox.Spr <62246052+Roxana-Spiridon@users.noreply.github.com>
2021-03-25 10:19:03 +0200

    YelpCamp.02

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const { campgroundSchema } = require('./schemas.js');
+const catchAsync = require('./utilities/catchAsync');
+const ExpressError = require('./utilities/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -19,12 +22,22 @@ db.once('open', () => {
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true })) //parseaza body-ul
-app.use(methodOverride('_method'));
-
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true })) //parseaza body-ul
+app.use(methodOverride('_method'));
+
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -38,33 +51,45 @@ app.get('/campgrounds/new', (req, res) => { // ruta spre pagina unde se creeaza 
     res.render('campgrounds/new');
 });
 
-app.post('/campgrounds', async(req, res) => { // se creeaza obiectul si se introduce in baza de date
+app.post('/campgrounds', validateCampground, catchAsync(async(req, res) => { // se creeaza obiectul si se introduce in baza de date
+    if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
-});
+}));
 
-app.get('/campgrounds/:id', async(req, res) => { // arata un obiect deja existent pe baza id-ului
+app.get('/campgrounds/:id', catchAsync(async(req, res) => { // arata un obiect deja existent pe baza id-ului
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground });
-});
+}));
 
-app.get('/campgrounds/:id/edit', async(req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async(req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/edit', { campground });
-});
+}));
 
-app.put('/campgrounds/:id', async(req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async(req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`);
-});
+}));
 
-app.delete('/campgrounds/:id', async(req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async(req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
-})
+}));
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
+});
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Ohh no! Something went wrong!';
+    res.status(statusCode).render('error', { err });
+});
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
