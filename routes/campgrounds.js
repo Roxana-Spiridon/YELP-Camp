49cd3d9d4cee89b5523b8ad0589ec36844201503
Rox.Spr <62246052+Roxana-Spiridon@users.noreply.github.com>
2021-03-30 11:34:04 +0300

    Configuring Session & Flash Messages

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -27,32 +27,42 @@ router.get('/new', (req, res) => { // ruta spre pagina unde se creeaza un obiect
 });
 
 router.post('/', validateCampground, catchAsync(async(req, res) => { // se creeaza obiectul si se introduce in baza de date
-    if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
-
+    //if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
+    req.flash('success', 'Successfully create a new campground!');
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
 router.get('/:id', catchAsync(async(req, res) => { // arata un obiect deja existent pe baza id-ului
     const campground = await Campground.findById(req.params.id).populate('reviews');
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground!');
+        return res.redirect('/campgrounds');
+    }
     res.render('campgrounds/show', { campground });
 }));
 
 router.get('/:id/edit', catchAsync(async(req, res) => {
     const campground = await Campground.findById(req.params.id);
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground!');
+        return res.redirect('/campgrounds');
+    }
     res.render('campgrounds/edit', { campground });
 }));
 
 router.put('/:id', validateCampground, catchAsync(async(req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground });
+    req.flash('success', 'Successfully updated campground!');
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
 router.delete('/:id', catchAsync(async(req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
+    req.flash('success', 'Successfully deleted campground!');
     res.redirect('/campgrounds');
 }));
 
