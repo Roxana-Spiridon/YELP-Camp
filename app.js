e8181a85d14aafd5a11daf907987a34ad42d799f
Rox.Spr <62246052+Roxana-Spiridon@users.noreply.github.com>
2021-03-30 10:00:10 +0300

    Routes

--- app.js
+++ app.js
@@ -2,14 +2,11 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const { campgroundSchema, reviewSchema } = require('./schemas.js');
-const catchAsync = require('./utilities/catchAsync');
 const ExpressError = require('./utilities/ExpressError');
 const methodOverride = require('method-override');
-const Campground = require('./models/campground');
-const Review = require('./models/review');
 
-const campgrounds = require('./routes/campgrounds');
+const campgrounds = require('./routes/campgrounds'); //route campgrounds
+const reviews = require('./routes/reviews'); //route reviews
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -32,47 +29,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true })) //parseaza body-ul
 app.use(methodOverride('_method'));
 
-app.use('/campgrounds', campgrounds); //route and path
-
-const validateCampground = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(msg, 400);
-    } else {
-        next();
-    }
-}
-
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(msg, 400);
-    } else {
-        next();
-    }
-}
+app.use('/campgrounds', campgrounds); //route and path - prefixul utilizat pentru ruta campgrounds
+app.use('/campgrounds/:id/reviews', reviews) //route and path - prefixul utilizat pentru ruta reviews
 
 app.get('/', (req, res) => {
     res.render('home');
 });
-
-app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async(req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-}));
-
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async(req, res) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/campgrounds/${id}`);
-}));
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404));
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/campgrounds', async(req, res) => {
+const Campground = require('../models/campground');
+const { campgroundSchema } = require('../schemas.js'); //JOI schema
+
+const catchAsync = require('../utilities/catchAsync');
+const ExpressError = require('../utilities/ExpressError');
+
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
+router.get('/', async(req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
 });
 
-router.get('/campgrounds/new', (req, res) => { // ruta spre pagina unde se creeaza un obiect nou
+router.get('/new', (req, res) => { // ruta spre pagina unde se creeaza un obiect nou
     res.render('campgrounds/new');
 });
 
-router.post('/campgrounds', validateCampground, catchAsync(async(req, res) => { // se creeaza obiectul si se introduce in baza de date
+router.post('/', validateCampground, catchAsync(async(req, res) => { // se creeaza obiectul si se introduce in baza de date
     if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
 
     const campground = new Campground(req.body.campground);
@@ -18,23 +34,23 @@ router.post('/campgrounds', validateCampground, catchAsync(async(req, res) => { 
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
-router.get('/campgrounds/:id', catchAsync(async(req, res) => { // arata un obiect deja existent pe baza id-ului
+router.get('/:id', catchAsync(async(req, res) => { // arata un obiect deja existent pe baza id-ului
     const campground = await Campground.findById(req.params.id).populate('reviews');
     res.render('campgrounds/show', { campground });
 }));
 
-router.get('/campgrounds/:id/edit', catchAsync(async(req, res) => {
+router.get('/:id/edit', catchAsync(async(req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/edit', { campground });
 }));
 
-router.put('/campgrounds/:id', validateCampground, catchAsync(async(req, res) => {
+router.put('/:id', validateCampground, catchAsync(async(req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
-router.delete('/campgrounds/:id', catchAsync(async(req, res) => {
+router.delete('/:id', catchAsync(async(req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
