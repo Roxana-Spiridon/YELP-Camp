23e74050fc5262de970661a8b29f3b5ed5792cd1
Rox.Spr <62246052+Roxana-Spiridon@users.noreply.github.com>
2021-04-02 15:29:58 +0300

    Basic Authorization

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,21 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const Campground = require('../models/campground');
-const { campgroundSchema } = require('../schemas.js'); //JOI schema
-const { isLoggedIn } = require('../middlewareAuth');
+const { isLoggedIn, validateCampground, isAuthor } = require('../middlewareAuth');
 
 const catchAsync = require('../utilities/catchAsync');
 const ExpressError = require('../utilities/ExpressError');
 
-const validateCampground = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(msg, 400);
-    } else {
-        next();
-    }
-}
 
 router.get('/', async(req, res) => {
     const campgrounds = await Campground.find({});
@@ -27,15 +17,21 @@ router.get('/new', isLoggedIn, (req, res) => { // ruta spre pagina unde se creea
     res.render('campgrounds/new');
 });
 
-router.post('/', isLoggedIn, validateCampground, catchAsync(async(req, res) => { // se creeaza obiectul si se introduce in baza de date
+router.post('/', isLoggedIn, validateCampground, catchAsync(async(req, res) => { // se creeaza un obiect nou si se introduce in baza de date
     const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
     await campground.save();
     req.flash('success', 'Successfully create a new campground!');
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
 router.get('/:id', catchAsync(async(req, res) => { // arata un obiect deja existent pe baza id-ului
-    const campground = await Campground.findById(req.params.id).populate('reviews');
+    const campground = await Campground.findById(req.params.id).populate({
+        path: 'reviews',
+        populate: {
+            path: 'author'
+        }
+    }).populate('author');
     if (!campground) {
         req.flash('error', 'Cannot find that campground!');
         return res.redirect('/campgrounds');
@@ -43,8 +39,9 @@ router.get('/:id', catchAsync(async(req, res) => { // arata un obiect deja exist
     res.render('campgrounds/show', { campground });
 }));
 
-router.get('/:id/edit', isLoggedIn, catchAsync(async(req, res) => {
-    const campground = await Campground.findById(req.params.id);
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async(req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
     if (!campground) {
         req.flash('error', 'Cannot find that campground!');
         return res.redirect('/campgrounds');
@@ -52,14 +49,14 @@ router.get('/:id/edit', isLoggedIn, catchAsync(async(req, res) => {
     res.render('campgrounds/edit', { campground });
 }));
 
-router.put('/:id', isLoggedIn, validateCampground, catchAsync(async(req, res) => {
+router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(async(req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground });
     req.flash('success', 'Successfully updated campground!');
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
-router.delete('/:id', isLoggedIn, catchAsync(async(req, res) => {
+router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async(req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     req.flash('success', 'Successfully deleted campground!');
