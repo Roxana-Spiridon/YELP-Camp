d751e8b08e5122ab764d93559c82aa1d898ceabc
Rox.Spr <62246052+Roxana-Spiridon@users.noreply.github.com>
2021-04-07 17:34:17 +0300

    Cluster Map

--- seeds/index.js
+++ seeds/index.js
@@ -19,12 +19,19 @@ const sample = array => array[Math.floor(Math.random() * array.length)]; // retu
 
 const seedDB = async() => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 300; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            author: '60645b795043373890ba833b',
+            author: '60645b795043373890ba833b', //user ID
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            geometry: {
+                type: 'Point',
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude
+                ]
+            },
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis cupiditate reprehenderit quia pariatur sunt, ex excepturi voluptatem quisquam autem incidunt quidem eius quo, repellendus sequi voluptates nemo corrupti! Nobis, mollitia.',
             price,
